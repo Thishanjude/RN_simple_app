64b39c54082deeaa468301d8602bcea60688e62f
ThishanJude <thishan1997@gmail.com>
2024-02-28 17:34:52 +0530

    added a list of array and mapped to components and final push

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import FlatCards from './components/FlatCards';
 import ElevatedCards from './components/ElevatedCards';
 import FancyCard from './components/FancyCard';
 import ActionCard from './components/ActionCard';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
       <ScrollView>
         <FlatCards />
         <ElevatedCards />
-        <FancyCard/>
-        <ActionCard/>
+        <FancyCard />
+        <ContactList />
+        <ActionCard />
       </ScrollView>
     </SafeAreaView>
   );
